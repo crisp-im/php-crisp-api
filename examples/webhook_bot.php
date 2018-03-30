bf40f9a2af5cc78ca4403270d733f4192b6a8c05
Baptiste Jamin <baptistejamin@gmail.com>
2018-03-30 17:12:05 +0200

    Bot example

---
+++ examples/webhook_bot.php
@@ -0,0 +1,30 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$CrispClient = new Crisp();
+
+// Authenticate to API (identifier, key)
+$CrispClient->authenticate("7c3ef21c-1e04-41ce-8c06-5605c346f73e", "cc29e1a5086e428fcc6a697d5837a66d82808e65c5cce006fbf2191ceea80a0a");
+//$CrispClient->authenticate(identifier, key);
+
+// This is the received raw event
+$inputJSON = file_get_contents('php://input');
+// Let's decode this event as a PHP array
+$input = json_decode($inputJSON, TRUE);
+
+// When a visitor leaves a message
+if ($input["event"] == "message:send") {
+	$websiteId = $input["data"]["website_id"];
+	$sessionId = $input["data"]["session_id"];
+	$message = [
+		"text" => "This is a bot reply",
+		"type" => "text",
+		"from" => "operator",
+		"origin" => "chat"
+	];
+
+	$CrispClient->websiteConversations->sendMessage($websiteId, $sessionId, $message);
+}
+
+?>
