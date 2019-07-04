8eddf0dcb872cc0db229e3338213396733f8d941
Baptiste Jamin <baptistejamin@gmail.com>
2019-07-04 13:32:36 +0200

    Add a new example

---
+++ examples/send_picker_message.php
@@ -0,0 +1,35 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$CrispClient = new Crisp();
+
+// Authenticate to API (identifier, key)
+$CrispClient->authenticate("7c3ef21c-1e04-41ce-8c06-5605c346f73e", "cc29e1a5086e428fcc6a697d5837a66d82808e65c5cce006fbf2191ceea80a0a");
+//$CrispClient->authenticate(identifier, key);
+
+$websiteId = "WEBSITE_ID";
+$sessionId = "SESSION_ID";
+
+$message = [
+	"content" => [
+      "id" => "routing",
+      "text" => "What is your question about?",
+      "choices" => [[
+        "value" => "sales",
+        "label"=> "Sales",
+        "selected" => false
+      ], [
+        "value" => "tech",
+        "label" => "Tech",
+        "selected" => false
+      ]]
+    ],
+	"type" => "picker",
+	"from" => "operator",
+	"origin" => "chat"
+];
+
+$CrispClient->websiteConversations->sendMessage($websiteId, $sessionId, $message);
+
+?>
