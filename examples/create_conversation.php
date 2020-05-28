9f8fe06e8b3840aadcb6842aafb893dff06901e0
Maxime Pauvert <maximepvrt@gmail.com>
2020-05-28 12:27:19 +0200

    fixs

--- examples/create_conversation.php
+++ examples/create_conversation.php
@@ -5,17 +5,13 @@ require '../vendor/autoload.php';
 $CrispClient = new \Crisp\CrispClient();
 
 // Get the keys from https://go.crisp.chat/account/token/
-$CrispClient->authenticate(
-    "7c3ef21c-1e04-41ce-8c06-5605c346f73e",
-    "cc29e1a5086e428fcc6a697d5837a66d82808e65c5cce006fbf2191ceea80a0a"
-);
 //$CrispClient->authenticate(identifier, key);
 
 // Replace with your own website ID
 $website_id = "YOUR_WEBSITE_ID";
 
 $nickname = "John Doe";
-$email = "john.doe@gmail.com"];
+$email = "john.doe@gmail.com";
 $content_message = "Hello World";
 
 $conversation = $CrispClient->websiteConversations->create($website_id);
--- examples/send_picker_message.php
+++ examples/send_picker_message.php
@@ -5,10 +5,6 @@ require '../vendor/autoload.php';
 $CrispClient = new \Crisp\CrispClient();
 
 // Get the keys from https://go.crisp.chat/account/token/
-$CrispClient->authenticate(
-    "7c3ef21c-1e04-41ce-8c06-5605c346f73e",
-    "cc29e1a5086e428fcc6a697d5837a66d82808e65c5cce006fbf2191ceea80a0a"
-);
 //$CrispClient->authenticate(identifier, key);
 
 $websiteId = "WEBSITE_ID";
