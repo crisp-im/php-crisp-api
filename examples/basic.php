497003ff6dfa954e1a1d45de182abde30f809f02
Baptiste Jamin <baptistejamin@gmail.com>
2020-10-23 12:53:42 +0200

    Cleanup

--- examples/basic.php
+++ examples/basic.php
@@ -5,11 +5,8 @@ require '../vendor/autoload.php';
 $CrispClient = new \Crisp\CrispClient();
 
 // Get the keys from https://go.crisp.chat/account/token/
-$CrispClient->authenticate(
-    "7c3ef21c-1e04-41ce-8c06-5605c346f73e",
-    "cc29e1a5086e428fcc6a697d5837a66d82808e65c5cce006fbf2191ceea80a0a"
-);
-//$CrispClient->authenticate(identifier, key);
+
+$CrispClient->authenticate(identifier, key);
 
 try {
     $profile = $CrispClient->userProfile->get();
